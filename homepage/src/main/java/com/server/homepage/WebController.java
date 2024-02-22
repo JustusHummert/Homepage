5fd70016db01de6c26c2dba7b2a09af38411c54e
Justus Hummert <justusjhummert@aol.com>
2024-02-22 18:34:41 +0100

    website that shows pdf and has ssl certificate

---
+++ homepage/src/main/java/com/server/homepage/WebController.java
@@ -0,0 +1,11 @@
+package com.server.homepage;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+    @GetMapping("")
+    public String homepage(){
+        return "homepage";
+    }
+}
